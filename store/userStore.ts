3c25a4b69c8987f0a6a8b5f0df595fa72f90b6a5
Soham-Kale <sohamkale3014@gmail.com>
2026-05-14 16:35:29 +0530

    create Create component and update userStore to use isAdmin state

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,11 +1,11 @@
 import { create } from "zustand";
 
 interface UserStore {
-    idAdmin: boolean;
+    isAdmin: boolean;
     setIsAdmin: (value: boolean) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
-    idAdmin: false,
-    setIsAdmin: (value: boolean) => set({ idAdmin: value }),
+    isAdmin: false,
+    setIsAdmin: (value: boolean) => set({ isAdmin: value }),
 }));
